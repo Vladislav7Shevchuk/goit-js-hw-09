8e4775576a34b73c362893253fb8ca1d2bb03132
Vladislav7Shevchuk <shewa123321@gmail.com>
2025-01-31 13:23:01 +0000

    Deploying to gh-pages from @ Vladislav7Shevchuk/goit-js-hw-09@eca4e741aed6bc564b5d1278faf405789645d0ff 🚀

--- 2-form.js
+++ 2-form.js
@@ -1,2 +1,2 @@
-import"./assets/styles-DD3qoKza.js";console.log("Form");
+import"./assets/styles-D7t41GzV.js";function s(e,t){const a=JSON.stringify(t);localStorage.setItem(e,a)}const n={form:document.querySelector(".feedback-form")};n.form.addEventListener("input",e=>{const t=e.currentTarget.elements.email.value,a=e.currentTarget.elements.message.value;s("feedback-form-state",{email:t,message:a})});
 //# sourceMappingURL=2-form.js.map
