eca4e741aed6bc564b5d1278faf405789645d0ff
Vladislav7Shevchuk <shewa123321@gmail.com>
2025-01-31 15:22:36 +0200

    js-9

--- src/js/2-fofm.js
+++ src/js/2-fofm.js
@@ -1,1 +1,25 @@
-console.log('Form');
+function saveToLS(key, value) {
+  const jsonData = JSON.stringify(value);
+  localStorage.setItem(key, jsonData);
+}
+
+function loadFromLS(key) {
+  const body = localStorage.getItem(key);
+  try {
+    const data = JSON.parse(body);
+    return data;
+  } catch {
+    return body;
+  }
+}
+
+const refs = {
+  form: document.querySelector('.feedback-form'),
+};
+
+refs.form.addEventListener('input', e => {
+  const email = e.currentTarget.elements.email.value;
+  const message = e.currentTarget.elements.message.value;
+  const data = { email, message };
+  saveToLS('feedback-form-state', data);
+});
